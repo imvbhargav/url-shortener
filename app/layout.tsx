ff09db3ad98460ea9985ae9d11349362aa969cc6
imvbhargav <87763304+imvbhargav@users.noreply.github.com>
2025-01-02 21:41:39 +0530

    Changed Layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,6 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
